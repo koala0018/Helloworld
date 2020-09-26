e5f05e73be18d4e4710bf9556af67297d58908bb
蜂蜜抽子 <66301991+koala0018@users.noreply.github.com>
2020-09-26 17:28:31 +0800

    最基础的模板设计模式

---
+++ 模板设计模式/Student.java
@@ -0,0 +1,7 @@
+
+public class Student extends Person{
+
+	public void work(){
+		System.out.println("学生的工作是学习");
+	}
+}
---
+++ 模板设计模式/Worker.java
@@ -0,0 +1,7 @@
+
+public class Worker extends Person{
+
+	public void work(){
+		System.out.println("工人的工作是加工生产");
+	}
+}
